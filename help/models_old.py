34cd1f3494a5a0ff4740708f4f3ff2ead24f5514
Pushin Alexander <Drakkar996@gmail.com>
2015-03-14 23:08:31 +0300

    Debuged and fully workable 'models.py'

--- help/models_old.py
+++ help/models_old.py
@@ -14,14 +14,14 @@ class User(db.Model):
         'mysql_charset': 'utf8'
     }
     id_user = db.Column(db.Integer(), db.ForeignKey('inst_profile.id_profile'),
-                        nullable=False, primary_key=True)
+                        unsigned=True, nullable=False, primary_key=True)
     first_name = db.Column(db.String(100))
     second_name = db.Column(db.String(100))
     registration_date = db.Column(db.DateTime, nullable=False,
                                   default=datetime.datetime.now())
     last_visit = db.Column(db.DateTime, nullable=False,
                                   default=datetime.datetime.now())
-    rating = db.Column(db.Integer, nullable=False, default=0)
+    rating = db.Column(db.Integer, unsigned=True, nullable=False, default=0)
 
     def __init__(self, id_user):
         self.id_user = id_user
@@ -35,7 +35,7 @@ class InstProfile(db.Model):
         'mysql_engine': 'InnoDB',
         'mysql_charset': 'utf8'
     }
-    id_profile = db.Column(db.Integer, nullable=False, primary_key=True)
+    id_profile = db.Column(db.Integer, unsigned=True, nullable=False, primary_key=True)
     login = db.Column(db.String(100), nullable=False, unique=True)
     #post_count = db.Column(db.Integer, unsigned=True, nullable=False)
     #followers_count = db.Column(db.Integer, unsigned=True, nullable=False)
@@ -49,16 +49,16 @@ class InstProfile(db.Model):
                               default=datetime.datetime.now())
 
     user = db.relationship('User', backref='inst_profile', lazy='dynamic')
-    followers = db.relationship('InstProfile', secondary='follows',
+    followers = db.relationship('InstProfile', secondary=follows,
         backref='following', lazy='dynamic')
-    words = db.relationship('Word', secondary='profile_words',
+    words = db.relationship('Word', secondary=profile_words,
         backref='inst_profiles', lazy='dynamic')
-    geos = db.relationship('Geo', secondary='profile_geos',
+    geos = db.relationship('Geo', secondary=profile_geos,
         backref='inst_profiles', lazy='dynamic')
     posts = db.relationship('Post', backref='ints_profile', lazy='dynamic')
-    filters = db.relationship('Filter', secondary='profile_filters',
+    filters = db.relationship('Filter', secondary=profile_filters,
         backref='inst_profiles', lazy='dynamic')
-    usermarks = db.relationship('InstProfile', secondary='usermarks',
+    usermarks = db.relationship('InstProfile', secondary=usermarks,
         backref='usermark_makers', lazy='dynamic')
 
     def __init__(self, id_profile, post_count, followers_count, following_count):
@@ -71,17 +71,17 @@ class InstProfile(db.Model):
         return '<InstProfile %r>' % self.login
 
 
-follows = db.Table('follows',
+follows = db.table('follows',
     db.Column('id_follower', db.Integer, db.ForeignKey('inst_profile.id_profile'),
-              nullable=False),
+              unsigned=True, nullable=False),
     db.Column('id_following', db.Integer, db.ForeignKey('inst_profile.id_profile'),
-              nullable=False),
+              unsigned=True, nullable=False),
     UniqueConstraint('id_follower', 'id_following'),
 
-    db.Column('given_like', db.Integer),
-    db.Column('given_comment', db.Integer),
-    db.Column('last_check', db.DateTime, nullable=False,
-                              default=datetime.datetime.now()))
+    given_like = db.Column(db.Integer, unsigned=True),
+    given_comment = db.Column(db.Integer, unsigned=True),
+    last_check = db.Column(db.DateTime, nullable=False),
+                              default=datetime.datetime.now())
 
 
 class Geo(db.Model):
@@ -89,7 +89,7 @@ class Geo(db.Model):
         'mysql_engine': 'InnoDB',
         'mysql_charset': 'utf8'
     }
-    id_geo = db.Column(db.Integer, nullable=False, primary_key=True)
+    id_geo = db.Column(db.Integer, unsigned=True, nullable=False, primary_key=True)
     geo_name = db.Column(db.String(100), nullable=False, unique=True)
     #using_count = db.Column(db.Integer, unsigned=True)
     #get_like = db.Column(db.Integer, unsigned=True)
@@ -103,16 +103,16 @@ class Geo(db.Model):
         return '<Geo %r>' % self.id_geo
 
 
-profile_geos = db.Table('profile_geos',
+profile_geos = db.table('profile_geos',
     db.Column('id_profile', db.Integer, db.ForeignKey('inst_profile.id_profile'),
-              nullable=False),
+              unsigned=True, nullable=False),
     db.Column('id_geo', db.Integer, db.ForeignKey('geo.id_geo'),
-               nullable=False),
+              unsigned=True, nullable=False),
     UniqueConstraint('id_profile', 'id_geo'),
 
-    db.Column('using_count', db.Integer),
-    db.Column('get_like', db.Integer),
-    db.Column('get_comment', db.Integer))
+    using_count = db.Column(db.Integer, unsigned=True),
+    get_like = db.Column(db.Integer, unsigned=True),
+    get_comment = db.Column(db.Integer, unsigned=True))
 
 
 class Word(db.Model):
@@ -120,14 +120,14 @@ class Word(db.Model):
         'mysql_engine': 'InnoDB',
         'mysql_charset': 'utf8'
     }
-    id_word = db.Column(db.Integer, nullable=False, primary_key=True,
+    id_word = db.Column(db.Integer, unsigned=True, nullable=False, primary_key=True,
                         autoincrement=True)
     word_name = db.Column(db.String(100), nullable=False, unique=True)
     #using_count = db.Column(db.Integer, unsigned=True)
     #get_like = db.Column(db.Integer, unsigned=True)
     #get_comment = db.Column(db.Integer, unsigned=True)
 
-    meanings = db.relationship('Meaning', secondary='word_meanings',
+    meanings = db.relationship('Meaning', secondary=word_meanings,
         backref='words', lazy='dynamic')
 
     def __init__(self, id_word, word_name):
@@ -138,17 +138,17 @@ class Word(db.Model):
         return '<Word %r>' % self.id_word
 
 
-profile_words = db.Table('profile_words',
+profile_words = db.table('profile_words',
     db.Column('id_profile', db.Integer, db.ForeignKey('inst_profile.id_profile'),
-              nullable=False),
+              unsigned=True, nullable=False),
     db.Column('id_word', db.Integer, db.ForeignKey('word.id_word'),
-              nullable=False),
+              unsigned=True, nullable=False),
     UniqueConstraint('id_profile', 'id_word'),
 
-    db.Column('using_count', db.Integer),
-    db.Column('get_like', db.Integer),
-    db.Column('get_comment', db.Integer),
-    db.Column('is_tag', db.Integer, nullable=False))
+    using_count = db.Column(db.Integer, unsigned=True),
+    get_like = db.Column(db.Integer, unsigned=True),
+    get_comment = db.Column(db.Integer, unsigned=True),
+    is_tag = db.Column(db.Boolean, unsigned=True, nullable=False))
 
 
 class Meaning(db.Model):
@@ -156,7 +156,7 @@ class Meaning(db.Model):
         'mysql_engine': 'InnoDB',
         'mysql_charset': 'utf8'
     }
-    id_meaning = db.Column(db.Integer, nullable=False, primary_key=True,
+    id_meaning = db.Column(db.Integer, unsigned=True, nullable=False, primary_key=True,
                            autoincrement=True)
     meaning_name = db.Column(db.String(100), nullable=False, unique=True)
     #using_count = db.Column(db.Integer, unsigned=True)
@@ -171,28 +171,28 @@ class Meaning(db.Model):
         return '<Meaning %r>' % self.id_meaning
 
 
-word_meanings = db.Table('word_meanings',
+word_meanings = db.table('word_meanings',
     db.Column('id_word', db.Integer, db.ForeignKey('word.id_word'),
-              nullable=False),
+              unsigned=True, nullable=False),
     db.Column('id_mean', db.Integer, db.ForeignKey('meaning.id_meaning'),
-              nullable=False),
-    UniqueConstraint('id_word', 'id_mean'),
+              unsigned=True, nullable=False),
+    UniqueConstraint('id_profile', 'id_geo'),
 
-    db.Column('importance', db.Integer, nullable=False, default=0))
+    importance = db.Column(db.Integer, unsigned=True, nullable=False, default=0))
 
 
-class Post(db.Model):
+class Posts(db.Model):
     __table_args__ = {
         'mysql_engine': 'InnoDB',
         'mysql_charset': 'utf8'
     }
-    id_post = db.Column(db.Integer, nullable=False, primary_key=True)
+    id_post = db.Column(db.Integer, unsigned=True, nullable=False, primary_key=True)
     id_profile = db.Column(db.Integer, db.ForeignKey('inst_profile.id_profile'),
-        nullable=False)
+        unsigned=True, nullable=False)
     UniqueConstraint('id_post', 'id_profile')
 
-    get_like = db.Column(db.Integer)
-    get_comment = db.Column(db.Integer)
+    get_like = db.Column(db.Integer, unsigned=True)
+    get_comment = db.Column(db.Integer, unsigned=True)
     img_url = db.Column(db.String(100), nullable=False, unique=True)
     filter = db.Column(db.String(100), nullable=False)
     last_check = db.Column(db.DateTime, nullable=False,
@@ -216,7 +216,7 @@ class Filter(db.Model):
         'mysql_engine': 'InnoDB',
         'mysql_charset': 'utf8'
     }
-    id_filter = db.Column(db.Integer, nullable=False, primary_key=True,
+    id_filter = db.Column(db.Integer, unsigned=True, nullable=False, primary_key=True,
                         autoincrement=True)
     filter_name = db.Column(db.String(100), nullable=False, unique=True)
     #using_count = db.Column(db.Integer, unsigned=True)
@@ -230,27 +230,27 @@ class Filter(db.Model):
         return '<Filter %r>' % self.id_filter
 
 
-profile_filters = db.Table('profile_filters',
+profile_filters = db.table('profile_filters',
     db.Column('id_profile', db.Integer, db.ForeignKey('inst_profile.id_profile'),
-              nullable=False),
+              unsigned=True, nullable=False),
     db.Column('id_filter', db.Integer, db.ForeignKey('filter.id_filter'),
-              nullable=False),
+              unsigned=True, nullable=False),
     UniqueConstraint('id_profile', 'id_filter'),
 
-    db.Column('using_count', db.Integer),
-    db.Column('get_like', db.Integer),
-    db.Column('get_comment', db.Integer))
+    using_count = db.Column(db.Integer, unsigned=True),
+    get_like = db.Column(db.Integer, unsigned=True),
+    get_comment = db.Column(db.Integer, unsigned=True))
 
 
-usermarks = db.Table('profile_usermarks',
+usermarks = db.table('profile_usermarks',
     db.Column('id_profile', db.Integer, db.ForeignKey('inst_profile.id_profile'),
-              nullable=False),
+              unsigned=True, nullable=False),
     db.Column('id_mark', db.Integer, db.ForeignKey('inst_profile.id_profile'),
-              nullable=False),
+              unsigned=True, nullable=False),
     UniqueConstraint('id_profile', 'id_mark'),
 
-    db.Column('id_post', db.Integer, db.ForeignKey('post.id_post'),
-              nullable=False),
-    db.Column('using_count', db.Integer),
-    db.Column('get_like', db.Integer),
-    db.Column('get_comment', db.Integer))
+    db.Column('id_post', db.Integer, db.ForeignKey('Posts.id_post'),
+              unsigned=True, nullable=False),
+    using_count = db.Column(db.Integer, unsigned=True),
+    get_like = db.Column(db.Integer, unsigned=True),
+    get_comment = db.Column(db.Integer, unsigned=True))
