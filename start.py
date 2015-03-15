c664579f9b88ac3a2705c0c806d720e66af080d3
alpus <drakkar996@gmail.com>
2015-03-15 14:55:58 +0300

    Structure of app changed

--- start.py
+++ start.py
@@ -1,16 +1,3 @@
-from flask import Flask
-from flask import render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('base.html')
-
-    
-@app.route('/<username>')
-def show_user_profile(username):
-    return render_template('base.html', username=username)
-
-if __name__ == "__main__":
-    app.run()
+#!flask/bin/python
+from app import app
+app.run(debug = True)
