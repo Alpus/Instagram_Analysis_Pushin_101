c3b6e6346add560277200070291a8674189cbed8
alpus <drakkar996@gmail.com>
2015-03-25 00:53:14 +0300

    make_virtualenv.bash changed

--- make_virtualenv.bash
+++ make_virtualenv.bash
@@ -2,7 +2,9 @@
 sudo pip install virtualenv
 sudo virtualenv flask
 sudo apt-get install mysql
-sudo apt-get install python-mysqldb
+sudo apt-get install python-dev
+sudo apt-get install libmysqlclient-dev 
+sudo flask/bin/pip install mysql-python
 sudo flask/bin/pip install flask
 sudo flask/bin/pip install flask-sqlalchemy
 sudo flask/bin/pip install flask-bootstrap
