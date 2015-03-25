65eef60e89ee69e8d9c3dbc4e36a098f8e72ee21
alpus <drakkar996@gmail.com>
2015-03-25 18:08:07 +0300

    Some mistakes corrected

--- app/logic.py
+++ app/logic.py
@@ -39,7 +39,7 @@ def process_login(code):
         that_inst_profile = inst_profile
 
 
-    that_user = that_inst_profile.user[0]
+    that_user = that_inst_profile.user
     if (that_user == None):
         user = models.User(id_user=instagram_user['id'],
                                access_token=access_token,
@@ -57,7 +57,7 @@ def process_login(code):
 def get_inst_profile(profile_id):
     inst_profile = db.session.query(models.InstProfile).filter(models.InstProfile.id_profile==
       profile_id).first()
-    user = inst_profile.user[0];
+    user = inst_profile.user
     api = InstagramAPI(access_token=user.access_token)
     user_data = api.user(user_id)
     inst_profile.bio = user_data['bio']
@@ -67,7 +67,7 @@ def get_inst_profile(profile_id):
 def get_profiles_posts(profile_id):
     inst_profile = db.session.query(models.InstProfile).filter(models.InstProfile.id_profile==
       profile_id).first()
-    user = inst_profile.user[0];
+    user = inst_profile.user
     api = InstagramAPI(access_token=user.access_token)
     posts_data = api.user(profile_id)
 '''    for post in posts_data:
