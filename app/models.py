f0a225f3d73468aa3ddd52e1d2814604c04f4784
alpus <drakkar996@gmail.com>
2015-05-01 18:12:27 +0300

    Unstable version with like analysis 006

--- app/models.py
+++ app/models.py
@@ -70,15 +70,15 @@ class Media(db.Model):
         'Location', backref=db.backref('medias', lazy='dynamic'))
     liked_by = db.relationship(
         'User', secondary='likes',
-        backref=db.backref('liked_media', lazy='dynamic'), lazy='dynamic')
+        backref=db.backref('liked_media', lazy='dynamic'))
     users_in_media = db.relationship(
         'User', secondary='marks',
-        backref=db.backref('media_with_user', lazy='dynamic'), lazy='dynamic')
+        backref=db.backref('media_with_user', lazy='dynamic'))
     comments = db.relationship(
         'Comment', backref=db.backref('medias', lazy='dynamic'))
     tags = db.relationship(
         'Tag', secondary='media_tags', backref=db.backref(
-        'medias_with_tag', lazy='dynamic'), lazy='dynamic')
+        'medias_with_tag', lazy='dynamic'))
 
     def __init__(self, media_data):
         id_media = int(media_data.id)
