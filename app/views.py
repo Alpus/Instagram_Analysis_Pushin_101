3ba8f58685a0932681b83894bb5d89858ca99eb4
Alpus <Drakkar996@gmai.com>
2015-05-23 04:24:51 +0300

    Small bug fixed 9

--- app/views.py
+++ app/views.py
@@ -22,16 +22,16 @@ def index():
     user_id = session.get('user_id', None)
 
     Analyse = forms.Analyse()
-    if Analyse.validate_on_submit():
-        requests.update_user(user_id)
-        requests.update_user_media(user_id)
-        requests.update_user_followed_by(user_id)
-        requests.update_user_follows(user_id)
-        return redirect('/analysis/'+str(user_id))
+    #if Analyse.validate_on_submit():
+    #    requests.update_user(user_id)
+    #    requests.update_user_media(user_id)
+    #    requests.update_user_followed_by(user_id)
+    #    requests.update_user_follows(user_id)
+    #    return redirect('/analysis/'+str(user_id))
 
     Login = forms.Login()
-    #if Login.validate_on_submit():
-    #    return redirect(login_url)
+    if Login.validate_on_submit():
+        return redirect(login_url)
 
     return render_template('login.html',
 
