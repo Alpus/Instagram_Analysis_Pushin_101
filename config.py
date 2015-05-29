220cbe2141f3eb25a411b7c9f47b96b2a5110d34
Alpus <Drakkar996@gmai.com>
2015-05-29 14:30:20 +0300

    auto refresh fixed

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'Pty1Ruz78LmG4jD8lAI36Fs'
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-STATIC_FOLDER = os.path.join(basedir, 'app', 'static')
+#STATIC_FOLDER = os.path.join(basedir, 'app', 'static')
 SQLALCHEMY_DATABASE_URI = 'mysql://alpus:Qwe61283888@localhost/db'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 CELERY_BROKER_URL='redis://localhost:6379',
