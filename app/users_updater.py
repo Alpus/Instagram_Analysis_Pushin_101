9eca7003974766af16141e6048b3b33728c1b0a3
Alpus <Drakkar996@gmai.com>
2015-05-30 15:25:26 +0300

    Demon fix 10

--- app/users_updater.py
+++ app/users_updater.py
@@ -7,14 +7,13 @@ import datetime
 
 def update_users_media():
     while True:
-        users = db.session.query(models.User).filter(models.User.access_token != None,
+        user = db.session.query(models.User).filter(models.User.access_token != None,
                                                      models.User.is_media_on_update == False)\
-                                                     .order_by(models.User.last_check).all()
-        for user in users:
-            if user.last_check is None:
-                user.last_check = datetime.date(year=1814, month=7, day=19)
-                db.session.commit()
-            if datetime.datetime.now() - user.last_check > datetime.timedelta(hours=24):
-                user.is_media_on_update = True;
-                db.session.commit()
-                requests.update_user_media.delay(user.inst_id_user)
+                                                     .order_by(models.User.last_check).first()
+        if user.last_check is None:
+            user.last_check = datetime.date(year=1814, month=7, day=19)
+            db.session.commit()
+        if datetime.datetime.now() - user.last_check > datetime.timedelta(hours=24):
+            user.is_media_on_update = True;
+            db.session.commit()
+            requests.update_user_media.delay(user.inst_id_user).wait()
