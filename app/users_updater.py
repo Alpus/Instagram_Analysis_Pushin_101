b7358e87106e9eba8adc055ebe302727df8b241c
Alpus <Drakkar996@gmai.com>
2015-05-30 16:16:34 +0300

    Demon fix 18

--- app/users_updater.py
+++ app/users_updater.py
@@ -12,7 +12,8 @@ while True:
     if user.last_check is None:
         user.last_check = datetime.date(year=1814, month=7, day=19)
         db.session.commit()
-    if datetime.datetime.now() - user.last_check > datetime.timedelta(hours=24):
+    if datetime.datetime.now() - user.last_check > datetime.timedelta(hours=24) and\
+        requests.is_access_token_valid(user.inst_id_user):
         user.is_media_on_update = True;
         db.session.commit()
         task = requests.update_user_media.delay(user.inst_id_user)
