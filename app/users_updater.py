27e41e2453ac8197526ff75eee2af7c921499344
Alpus <Drakkar996@gmai.com>
2015-05-30 18:34:17 +0300

    Demon fixed

--- app/users_updater.py
+++ app/users_updater.py
@@ -5,25 +5,25 @@ import datetime
 
 
 while True:
-    print 'new user'
-    dbsession = db.sessionmaker()()
-    user = dbsession.query(models.User).filter(models.User.access_token != None,
+    #print 'new user'
+    user = db.session.query(models.User).filter(models.User.access_token != None,
                                                  models.User.is_media_on_update == False)\
                                                  .order_by(models.User.last_check).first()
-    print user.login
-    print user.last_check
-    print datetime.datetime.now() - user.last_check > datetime.timedelta(hours=24)
-    print user.access_token
-    print requests.is_access_token_valid(user.inst_id_user)
+    #print user.login
+    #print user.last_check
+    #print datetime.datetime.now() - user.last_check > datetime.timedelta(hours=24)
+    #print user.access_token
+    #print requests.is_access_token_valid(user.inst_id_user)
     if user.last_check is None:
         user.last_check = datetime.date(year=1814, month=7, day=19)
         db.session.commit()
     if datetime.datetime.now() - user.last_check > datetime.timedelta(hours=24) and\
         requests.is_access_token_valid(user.inst_id_user):
-        print 'process user'
+        #print 'process user'
         user.is_media_on_update = True
         db.session.commit()
         task = requests.update_user_media.delay(user.inst_id_user)
-        print 'waiting started'
+        #print 'waiting started'
         task.wait()
-        print 'waiting end'
+        print user.login + ' updated'
+        #print 'waiting end'
