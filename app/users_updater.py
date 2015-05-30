942c485f21839546888f19072d519892c8819b8a
Alpus <Drakkar996@gmai.com>
2015-05-31 01:24:22 +0300

    Bug fixed

--- app/users_updater.py
+++ app/users_updater.py
@@ -20,17 +20,24 @@ while True:
         db.session.commit()
     if datetime.datetime.now() - user.last_check > datetime.timedelta(hours=24) and\
         requests.is_access_token_valid(user.inst_id_user):
-        #print 'process user'
+        print user.login + ' started'
         user.is_media_on_update += 3
         db.session.commit()
-        task_media = requests.update_user_media.delay(user.inst_id_user)
-        task_followed_by = requests.update_user_followed_by.delay(user.inst_id_user)
-        task_follows = requests.update_user_follows.delay(user.inst_id_user)
 
-        task_media.wait()
+        task_followed_by = requests.update_user_followed_by.delay(user.inst_id_user)
+        print 'Followed by started'
+        task_follows = requests.update_user_follows.delay(user.inst_id_user)
+        print 'Follows started'
+        task_media = requests.update_user_media.delay(user.inst_id_user)
+        print 'Medias by started'
+
         task_followed_by.wait()
+        print 'Followed by ended'
         task_follows.wait()
+        print 'Follows ended'
+        task_media.wait()
+        print 'Medias ended'
 
         db.session.commit()
-        print user.login + ' updated'
+        print user.login + ' updated \n \n'
         #print 'waiting end'
