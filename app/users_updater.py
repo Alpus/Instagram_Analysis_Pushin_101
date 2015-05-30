2ea6f1f9f0ff050575e3dedc43cf5fccf3f647e8
Alpus <Drakkar996@gmai.com>
2015-05-30 17:26:16 +0300

    Test commit

--- app/users_updater.py
+++ app/users_updater.py
@@ -5,15 +5,20 @@ import datetime
 
 
 while True:
+    print 'new user'
     user = db.session.query(models.User).filter(models.User.access_token != None,
                                                  models.User.is_media_on_update == False)\
                                                  .order_by(models.User.last_check).first()
+    print 'new user get'
     if user.last_check is None:
         user.last_check = datetime.date(year=1814, month=7, day=19)
         db.session.commit()
     if datetime.datetime.now() - user.last_check > datetime.timedelta(hours=24) and\
         requests.is_access_token_valid(user.inst_id_user):
+        print 'process user'
         user.is_media_on_update = True
         db.session.commit()
         task = requests.update_user_media.delay(user.inst_id_user)
+        print 'waiting started'
         task.wait()
+        print 'waiting end'
