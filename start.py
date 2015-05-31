8e30ba134b35ea4dfb28765b1b1192438a5f9ed5
Alpus <Drakkar996@gmai.com>
2015-05-31 15:06:37 +0300

    uwsgi fixed

--- start.py
+++ start.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 from app import app
 
-#if __name__ == "__main__":
-app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
 #app.run(debug=False, port=5000)
